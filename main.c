0d4ad0428742d8ae9ac30cc77d1f469b5bfafb2f
magonazz1 <martinmagonazz@outlook.com>
2023-11-18 12:23:51 -0800

    uplink - tasks

--- main.c
+++ main.c
@@ -1,46 +1,55 @@
 #include "monty.h"
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 /**
- * main - Entry point for Monty Bytecode Interpreter
- * @argc: The count of command-line arguments
- * @argv: An array of strings containing the command-line arguments
+ *  main - Main
  *
- * Description:
- * This function serves as the entry point for the Monty Bytecode Interpreter.
- * It takes the count of command-line arguments (argc) and an array of strings
- * (argv) containing the command-line arguments.
- * If the number of arguments is not exactly 2, it returns EXIT_FAILURE and
- * displays a usage error message.
- * It then attempts to open the Monty bytecode file specified in argv[1] for
- * reading. If the file cannot be opened, it returns EXIT_FAILURE and displays
- * an error message indicating the failed file open.
- * If the file is successfully opened, it proceeds to execute the Monty
- * interpreter logic by calling the run_monty function with the opened file.
- * Finally, it closes the opened file and returns the exit code from
- * run_monty, which can be EXIT_SUCCESS or EXIT_FAILURE.
+ *  @argc: Number of args
  *
- * Return:
- * - EXIT_SUCCESS on successful execution of the Monty interpreter.
- * - EXIT_FAILURE on errors such as incorrect usage or failed file open.
+ *  @argv: Command line args
+ *
+ *  Return: Void
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	FILE *script_fd = NULL;
-	int close_code = EXIT_SUCCESS;
+	stack_t *head = NULL;
+	char  *str = NULL, *operator_array[2], *temp;
+	size_t bufsize = 1024, line_count = 0;
+	ssize_t get_line;
+	void (*operator_function)(stack_t **stack, unsigned int line_number);
 
 	if (argc != 2)
+		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+	while (1)
 	{
-		return (usage_error());
+		get_line = getline(&str, &bufsize, file);
+		if (get_line == -1)
+			break;
+		line_count++;
+		operator_array[0] = strtok(str, "\n ");
+		if (operator_array[0] == NULL)
+			get_nop(&head, line_count);
+		else if (strcmp("push", operator_array[0]) == 0)
+		{
+			temp = strtok(NULL, "\n ");
+
+			get_push(&head, line_count, temp);
+		}
+		else if (operator_array[0] != NULL && operator_array[0][0] != '#')
+		{
+			operator_function = go(operator_array[0], line_count, &head);
+
+			if (operator_function == NULL && line_count == 0)
+			{
+				fprintf(stderr, "L%ld: unknown instruction %s\n",
+					line_count, operator_array[0]), exit(EXIT_FAILURE);
+			}
+		operator_function(&head, line_count);
+		}
 	}
-	script_fd = fopen(argv[1], "r");
-	if (script_fd == NULL)
-	{
-		return (f_open_error(argv[1]));
-	}
-	close_code = run_monty(script_fd);
-	fclose(script_fd);
-	return (close_code);
+	fclose(file), free(str), get_free(head);
+	return (0);
 }
